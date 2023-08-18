050c4473e9358a77bff0cec56ae3bd06851e22b6
llipengda <lipengda2@outlook.com>
2023-08-19 01:40:14 +0800

    add login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PDLiSiteAPI.Services;
+
+namespace PDLiSiteAPI.Controllers;
+
+[ApiController]
+[Route("Api/[controller]")]
+public class LoginController : ControllerBase
+{
+    private readonly ILogger<LoginController> _logger;
+
+    private readonly IConfiguration _configuration;
+
+    private readonly ILoginService _loginService;
+
+    public LoginController(
+        ILogger<LoginController> logger,
+        IConfiguration configuration,
+        ILoginService loginService
+    )
+    {
+        _logger = logger;
+        _configuration = configuration;
+        _loginService = loginService;
+    }
+
+    [HttpPost]
+    public ActionResult<string> Login(string password)
+    {
+        if (string.IsNullOrEmpty(password))
+        {
+            _logger.LogWarning(
+                "POST /Api/Login?password={password} ERROR Password can't be empty",
+                password
+            );
+            return BadRequest("Password can't be empty");
+        }
+        if (password.Trim() != _configuration["Password"])
+        {
+            _logger.LogWarning(
+                "POST /Api/Login?password={password} ERROR INCORRECT PASSWORD",
+                password
+            );
+            return Unauthorized("INCORRECT PASSWORD");
+        }
+        var token = _loginService.Generate();
+        _logger.LogInformation("POST /Api/Login?password={password} {token}", password, token);
+        return Ok(token);
+    }
+}
---
+++ Services/LoginService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PDLiSiteAPI.Services;
+
+public interface ILoginService
+{
+    string Generate();
+}
+
+public class LoginService : ILoginService
+{
+    private readonly IConfiguration _configuration;
+
+    public LoginService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string Generate()
+    {
+        var algorithm = SecurityAlgorithms.HmacSha256;
+
+        var claims = new[] { new Claim(ClaimTypes.Role, "Admin") };
+
+        var secretKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"])
+        );
+
+        var signingCredentials = new SigningCredentials(secretKey, algorithm);
+
+        var jwtSecurityToken = new JwtSecurityToken(
+            _configuration["Jwt:Issuer"],
+            _configuration["Jwt:Audience"],
+            claims,
+            DateTime.Now,
+            DateTime.Now.AddDays(30),
+            signingCredentials
+        );
+
+        var token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+        return token;
+    }
+}
