d43ebab2bd23fc70fd8b13b7c47c4721e0fd76b0
llipengda <lipengda2@outlook.com>
2023-08-21 00:23:57 +0800

    add log for loghub

--- Hubs/LogHub.cs
+++ Hubs/LogHub.cs
@@ -1,6 +1,25 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using PDLiSiteAPI.Models;
 
 namespace PDLiSiteAPI.Hubs;
 
-public class LogHub : Hub { }
+public class LogHub : Hub
+{
+    private readonly ILogger<LogHub> _logger;
+
+    public LogHub(ILogger<LogHub> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        _logger.LogInformation("[SignalR] LogHub is Connected");
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        _logger.LogInformation("[SignalR] LogHub is Disonnected");
+        return base.OnDisconnectedAsync(exception);
+    }
+}
